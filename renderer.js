7d4ad3e518e60ee94974eaaded7ef2861f10622a
Ethan Xie <79242981+Anonymouseyy@users.noreply.github.com>
2024-03-11 20:55:40 -0700

    trying my best

---
+++ renderer.js
@@ -0,0 +1,57 @@
+main();
+
+function createShader(gl, type, source) {
+    var shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+    if (success) {
+      return shader;
+    }
+   
+    console.log(gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+}
+
+function createProgram(gl, fragmentShader) {
+    var program = gl.createProgram();
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+    var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+    if (success) {
+      return program;
+    }
+   
+    console.log(gl.getProgramInfoLog(program));
+    gl.deleteProgram(program);
+}
+
+function main() {
+    var canvas = document.getElementById("canvas");
+    var source = `
+    void mainImage( out vec4 fragColor, in vec2 fragCoord ) {
+        // Normalized pixel coordinates (from 0 to 1)
+        vec2 uv = fragCoord/iResolution.xy;
+
+        // Time varying pixel color
+        vec3 col = 0.5 + 0.5*cos(iTime+uv.xyx+vec3(0,2,4));
+
+        // Output to screen
+        fragColor = vec4(col,1.0);
+    }`
+    canvas.width  = window.innerWidth;
+    canvas.height = window.innerHeight;
+    // Initialize the GL context
+    var gl = canvas.getContext("webgl");
+
+    // Only continue if WebGL is available and working
+    if (gl === null) {
+        alert(
+            "Unable to initialize WebGL. Your browser or machine may not support it.",
+        );
+        return;
+    }
+
+    var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, source);
+    var program = createProgram(gl, fragmentShader);
+}
